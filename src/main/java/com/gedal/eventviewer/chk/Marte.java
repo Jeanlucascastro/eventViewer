5c1604c000deb0f02f631f4a5f321fcea1c9c740
Jean Lucas de Castro <jeanlucascastro@gmail.com>
2022-05-30 08:19:31 -0300

    Some bug fixies

--- src/main/java/com/gedal/eventviewer/chk/Marte.java
+++ src/main/java/com/gedal/eventviewer/chk/Marte.java
@@ -1,5 +1,20 @@
 package com.gedal.eventviewer.chk;
 
-public class Marte {
+public class Marte implements Astro{
+	
+	Double size;
+	Double distance;
+	
+	@Override
+	public Double size(double size) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public Double distance(double distance) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 
 }
